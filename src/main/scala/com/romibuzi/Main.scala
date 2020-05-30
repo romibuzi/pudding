e74ced9862a9bc470495490097b5ad92e93f1925
Romain Ardiet <romain.ardiet@gmail.com>
2020-05-30 17:53:45 +0200

    bump to ZIO#1.0.0-RC20 (ExitCode instead of Int for programs returns)

--- src/main/scala/com/romibuzi/Main.scala
+++ src/main/scala/com/romibuzi/Main.scala
@@ -1,10 +1,10 @@
 package com.romibuzi
 
-import zio.ZIO
+import zio.{ExitCode, ZIO}
 import zio.console.putStrLn
 
 object Main extends zio.App {
-  override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] = {
-    putStrLn("Hello world!") *> ZIO.succeed(0)
+  override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, ExitCode] = {
+    putStrLn("Hello world!") *> ZIO.succeed(ExitCode.success)
   }
 }
--- src/main/scala/com/romibuzi/ratelimiter/TestRateLimiter.scala
+++ src/main/scala/com/romibuzi/ratelimiter/TestRateLimiter.scala
@@ -1,23 +1,23 @@
 package com.romibuzi.ratelimiter
 
-import zio.ZIO
+import zio.{ExitCode, ZIO}
 import zio.clock.Clock
 import zio.console.{Console, putStrLn}
 
 object Program {
-  def apply(): ZIO[Console with Clock, Nothing, Int] = {
+  def apply(): ZIO[Console with Clock, Nothing, ExitCode] = {
     for {
       limiter <- RateLimiter.make(perSecond = 1, buffer = 990)
       _ <- ZIO.foreach(1 to 1000) { i =>
         limiter.rateLimit(putStrLn(i.toString))
       }
-    } yield 0
+    } yield ExitCode.success
   }
 }
 
 // https://medium.com/wix-engineering/building-a-super-easy-rate-limiter-with-zio-88f1ccb49776
 object TestRateLimiter extends zio.App {
-  override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] =
+  override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, ExitCode] =
     Program.apply()
 }
 
